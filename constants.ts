ccf4d8f43311f32d26ecb7e26c57f4b34ec8adb8
Henrique Lima <henrique@example.com>
2026-02-10 09:51:40 -0300

    Revert "Update pricing logic based on weight and update price table image"
    This reverts commit 8d143b71a7e33828d081d9a8fe2061cc553a270b.

--- constants.ts
+++ constants.ts
@@ -52,13 +52,13 @@ export const PET_WEIGHT_OPTIONS: Record<PetWeight, string> = {
 // Preços base por peso: Banho e Só Tosa (tosa isolada)
 // Observação: O preço de "Banho & Tosa" é a soma Banho + Só Tosa
 export const SERVICE_PRICES: Record<PetWeight, { [key in ServiceType.BATH | ServiceType.GROOMING_ONLY]: number }> = {
-  [PetWeight.UP_TO_5]: { [ServiceType.BATH]: 70, [ServiceType.GROOMING_ONLY]: 65 },
-  [PetWeight.KG_10]: { [ServiceType.BATH]: 80, [ServiceType.GROOMING_ONLY]: 75 },
-  [PetWeight.KG_15]: { [ServiceType.BATH]: 90, [ServiceType.GROOMING_ONLY]: 85 },
-  [PetWeight.KG_20]: { [ServiceType.BATH]: 100, [ServiceType.GROOMING_ONLY]: 95 },
-  [PetWeight.KG_25]: { [ServiceType.BATH]: 120, [ServiceType.GROOMING_ONLY]: 115 },
-  [PetWeight.KG_30]: { [ServiceType.BATH]: 160, [ServiceType.GROOMING_ONLY]: 145 },
-  [PetWeight.OVER_30]: { [ServiceType.BATH]: 180, [ServiceType.GROOMING_ONLY]: 160 },
+  [PetWeight.UP_TO_5]: { [ServiceType.BATH]: 65, [ServiceType.GROOMING_ONLY]: 65 },
+  [PetWeight.KG_10]: { [ServiceType.BATH]: 75, [ServiceType.GROOMING_ONLY]: 75 },
+  [PetWeight.KG_15]: { [ServiceType.BATH]: 85, [ServiceType.GROOMING_ONLY]: 85 },
+  [PetWeight.KG_20]: { [ServiceType.BATH]: 95, [ServiceType.GROOMING_ONLY]: 95 },
+  [PetWeight.KG_25]: { [ServiceType.BATH]: 105, [ServiceType.GROOMING_ONLY]: 105 },
+  [PetWeight.KG_30]: { [ServiceType.BATH]: 115, [ServiceType.GROOMING_ONLY]: 115 },
+  [PetWeight.OVER_30]: { [ServiceType.BATH]: 150, [ServiceType.GROOMING_ONLY]: 150 },
 };
 
 export const ADDON_SERVICES: AddonService[] = [
